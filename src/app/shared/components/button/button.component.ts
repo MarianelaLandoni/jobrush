ae80fcddde8e08bedbc5a36f88cc7e531d6b0829
Marianela Landoni <mlandonih@hotmail.com>
2025-01-02 17:24:14 +0100

    feat: add primary button component

---
+++ src/app/shared/components/button/button.component.ts
@@ -0,0 +1,16 @@
+import { NgClass } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [NgClass],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss',
+})
+export class ButtonComponent {
+  @Input() text = 'Botón';
+  @Input() disabled = false;
+
+  isHover = false;
+}
