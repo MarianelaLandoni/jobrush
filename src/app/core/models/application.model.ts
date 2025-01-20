1e430f043c966f3b1ddbfd9e1d4de8278dcd4489
Marianela Landoni <mlandonih@hotmail.com>
2025-01-20 10:52:14 +0100

    feat: update application model

--- src/app/core/models/application.model.ts
+++ src/app/core/models/application.model.ts
@@ -2,7 +2,13 @@ export interface Application {
   id: number;
   title: string;
   company: string;
+  location: string;
+  image_url?: string;
+  url: string;
+  description?: string;
+  platform?: string;
+  boardId: number;
   status: string;
+  isHover?: boolean;
   created_at: string;
-  image_url: string;
 }
