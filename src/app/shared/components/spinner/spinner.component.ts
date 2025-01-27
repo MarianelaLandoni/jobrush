f10922acd9a66c2e0e7ea883243bd6791db6b416
Marianela Landoni <mlandonih@hotmail.com>
2025-01-27 10:16:09 +0100

    feat: add spinner, interceptor and service

---
+++ src/app/shared/components/spinner/spinner.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-spinner',
+  standalone: true,
+  imports: [],
+  template: `<div class="loader"></div>`,
+  styleUrl: './spinner.component.scss',
+})
+export class SpinnerComponent {
+
+}
