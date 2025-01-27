8c3f92152089cd3bb309f8ecd178cd81d663fc57
Marianela Landoni <mlandonih@hotmail.com>
2025-01-27 10:16:24 +0100

    feat: add spinnerInterceptor to app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,7 @@ import { provideRouter, withComponentInputBinding, withViewTransitions } from '@
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from 'core/interceptors/auth.interceptor';
+import { spinnerInterceptor } from 'core/interceptors/spinner.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,6 +12,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes, withComponentInputBinding(), withViewTransitions()),
     provideHttpClient(
       withFetch(),
-      withInterceptors([authInterceptor])),
+      withInterceptors([authInterceptor, spinnerInterceptor])),
   ],
 };
